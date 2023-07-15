fffca9769739f20b5de546743fb3eb898ad37464
henribrit <henriquezbrt@gmail.com>
2023-07-15 00:02:49 -0300

    initial

---
+++ fish/alias.fish
@@ -0,0 +1,29 @@
+# EXA
+alias lss "exa --long --all --icons --header"
+
+# BRIGHTNESS
+abbr bright "brightnessctl s"
+
+# PECO
+alias p "peco_select_history"
+
+#CLEAR
+alias c "clear"
+
+#CD
+alias c. "cd .."
+alias ch "cd ~"
+alias chc "cd ~ & clear"
+
+#GH
+alias ghauth "gh auth login"
+
+#GIT (g)
+abbr go 'git remote add origin https://github.com/henribrit/'
+abbr gc "git commit -m '"
+abbr ga git add
+abbr gp "git push -u origin"
+
+#NEO
+
+abbr matrix1 "neo -a -b 2 -c red -d 99 -G 50 -g 100,150 -M1 --shortpct=100 -m '"
